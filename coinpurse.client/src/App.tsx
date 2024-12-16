5af2f2e242fc4b657765ce5bcfa52e7fb1fa6f6c
Tom Powers <17350105+tpow4@users.noreply.github.com>
2024-12-15 22:17:29 -0600

    frontend MUI boilerplate

--- coinpurse.client/src/App.tsx
+++ coinpurse.client/src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { LineChart } from "@mui/x-charts/LineChart";
+import "./App.css";
+import { Box, Container, Grid2, Stack, Typography } from "@mui/material";
+import FinancialChart from "./FinancialChart";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box sx={{ display: "flex" }}>
+      {/* <SideMenu /> */}
+      {/* <AppNavbar /> */}
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+          pt: 8,
+          px: 3,
+        }}
+      >
+        <Stack
+          spacing={2}
+          sx={{
+            alignItems: "center",
+            mx: 3,
+            pb: 5,
+            mt: { xs: 8, md: 0 },
+          }}
+        >
+          {/* <Header /> */}
+          {/* <MainGrid /> */}
+          <Container maxWidth="lg">
+            <Grid2
+              container
+              spacing={2}
+              columns={12}
+              sx={{ mb: (theme) => theme.spacing(2) }}
+            >
+              <Grid2 size={{ xs: 12, md: 6 }}>
+                <FinancialChart/>
+              </Grid2>
+              <Grid2 size={{ xs: 12, md: 6 }}>
+                <Typography>Test Graph</Typography>
+                <LineChart
+                  xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                  series={[
+                    {
+                      data: [2, 5.5, 2, 8.5, 1.5, 5],
+                    },
+                  ]}
+                  width={500}
+                  height={300}
+                />
+              </Grid2>
+            </Grid2>
+          </Container>
+        </Stack>
+      </Box>
+    </Box>
+  );
 }
 
-export default App
+export default App;
--- coinpurse.client/src/main.tsx
+++ coinpurse.client/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './Theme/Theme.tsx'
+import CssBaseline from '@mui/material/CssBaseline/CssBaseline'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+    </ThemeProvider>
+  </StrictMode>
+);
