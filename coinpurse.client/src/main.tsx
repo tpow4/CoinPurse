96b0fce512b8185bf6c8cb2ff22e36bc010ff5a3
Tom Powers <17350105+tpow4@users.noreply.github.com>
2025-04-04 22:49:54 -0700

    dependency updates & redux store

--- coinpurse.client/src/main.tsx
+++ coinpurse.client/src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './containers/App.tsx'
 import { ThemeProvider } from '@mui/material/styles'
-import theme from './Theme/Theme.tsx'
 import CssBaseline from '@mui/material/CssBaseline/CssBaseline'
 import { store } from './redux/store.ts'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router'
+import theme from './theme/theme.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
--- coinpurse.client/src/redux/store.ts
+++ coinpurse.client/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import accountsReducer from './slices/accountsSlice';
+import balancesReducer from './slices/balancesSlice';
 
 export const store = configureStore({
     reducer: {
         accounts: accountsReducer,
+        balances: balancesReducer
     },
 });
 
