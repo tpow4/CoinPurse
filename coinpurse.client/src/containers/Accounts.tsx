741d6acf3774a42bb27a745d97b683dd1037d294
Tom Powers <17350105+tpow4@users.noreply.github.com>
2025-05-17 22:27:45 -0700

    implement account charts

--- coinpurse.client/src/containers/Accounts.tsx
+++ coinpurse.client/src/containers/Accounts.tsx
@@ -1,26 +1,54 @@
 import { useEffect } from "react";
-import { fetchAccounts, selectAccountsError, selectAccountsStatus, selectAllAccounts } from "../redux/slices/accountsSlice";
-import { Stack, Typography } from "@mui/material";
+import {
+    fetchAccounts,
+    selectAccountsError,
+    selectAccountsStatus,
+    selectAllAccounts,
+} from "../redux/slices/accountsSlice";
+import { Grid2 } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import {
+    fetchBalances,
+    selectAllBalances,
+    selectBalancesStatus,
+} from "../redux/slices/balancesSlice";
+import AccountChart from "./AccountChart";
 
 export default function Accounts() {
     const dispatch = useAppDispatch();
-    const accounts = useAppSelector(selectAllAccounts)
-    const accountStatus = useAppSelector(selectAccountsStatus)
-    const accountError = useAppSelector(selectAccountsError)
+    const accounts = useAppSelector(selectAllAccounts);
+    const accountStatus = useAppSelector(selectAccountsStatus);
+    const accountError = useAppSelector(selectAccountsError);
+    const balances = useAppSelector(selectAllBalances);
+    const balanceStatus = useAppSelector(selectBalancesStatus);
+    const balanceError = useAppSelector(selectAccountsError);
 
     useEffect(() => {
         dispatch(fetchAccounts());
+        dispatch(fetchBalances());
     }, [dispatch]);
 
-    if (accountStatus === 'pending') return <p>Loading...</p>;
-    if (accountStatus === 'rejected') return <p>Error: {accountError}</p>;
-
+    if (accountStatus === "pending" || balanceStatus === "pending")
+        return <p>Loading...</p>;
+    if (accountStatus === "rejected") return <p>Error: {accountError}</p>;
     return (
-        <Stack>
+        <Grid2
+            container
+            spacing={2}
+            columns={12}
+            sx={{ mb: (theme) => theme.spacing(2) }}
+        >
             {accounts.map((account) => (
-                <Typography key={account.id}>{account.name}</Typography>
+                <Grid2 key={account.id} size={{ xs: 12, md: 6 }}>
+                    <AccountChart
+                        key={account.id}
+                        account={account}
+                        balances={balances.filter(
+                            (b) => b.accountId === account.id
+                        )}
+                    />
+                </Grid2>
             ))}
-        </Stack>
+        </Grid2>
     );
 }
--- coinpurse.client/src/containers/Pages/DashboardPage.tsx
+++ coinpurse.client/src/containers/Pages/DashboardPage.tsx
@@ -1,7 +1,5 @@
-import { LineChart } from "@mui/x-charts/LineChart";
 import "../../App.css";
-import { Container, Grid2, Stack, Typography } from "@mui/material";
-import FinancialChart from "../FinancialChart";
+import { Container, Stack } from "@mui/material";
 import Accounts from "../Accounts";
 
 function DashboardPage() {
@@ -18,32 +16,7 @@ function DashboardPage() {
             {/* <Header /> */}
             {/* <MainGrid /> */}
             <Container maxWidth="lg">
-                <Grid2
-                    container
-                    spacing={2}
-                    columns={12}
-                    sx={{ mb: (theme) => theme.spacing(2) }}
-                >
-                    <Grid2 size={{ xs: 12, md: 6 }}>
-                        <FinancialChart />
-                    </Grid2>
-                    <Grid2 size={{ xs: 12, md: 6 }}>
-                        <Typography>Test Graph</Typography>
-                        <LineChart
-                            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-                            series={[
-                                {
-                                    data: [2, 5.5, 2, 8.5, 1.5, 5],
-                                },
-                            ]}
-                            width={500}
-                            height={300}
-                        />
-                    </Grid2>
-                    <Grid2 size={{ xs: 12, md: 6 }}>
-                        <Accounts />
-                    </Grid2>
-                </Grid2>
+                <Accounts />
             </Container>
         </Stack>
     );
