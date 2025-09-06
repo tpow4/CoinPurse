09b40bb166b29e9851c51ff1911561aa69d21500
Tom Powers <17350105+tpow4@users.noreply.github.com>
2025-09-06 15:01:50 -0700

    add monthly tracker ui

--- coinpurse.client/src/containers/App.tsx
+++ coinpurse.client/src/containers/App.tsx
@@ -3,6 +3,7 @@ import DashboardPage from "./Pages/DashboardPage";
 import SettingsPage from "./Pages/SettingsPage";
 import AppLayout from "./AppLayout";
 import CheckupPage from "./Pages/CheckupPage";
+import MonthlyBalanceTracker from "./Pages/MonthlyBalanceTracker";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route index element={<CheckupPage />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
                 <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/tracker" element={<MonthlyBalanceTracker />} />
             </Route>
         </Routes>
     );
