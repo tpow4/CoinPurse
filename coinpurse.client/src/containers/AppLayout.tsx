8e4519586866cddb71bbe15fb8fc88d5008cd836
Tom Powers <17350105+tpow4@users.noreply.github.com>
2025-09-06 15:04:01 -0700

    fix scroll so it is on far side of content

--- coinpurse.client/src/containers/AppLayout.tsx
+++ coinpurse.client/src/containers/AppLayout.tsx
@@ -1,29 +1,35 @@
 import { Outlet } from "react-router-dom";
 import TopAppBar from "./TopAppBar";
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const Layout = () => (
-    <Box sx={{ 
-        display: "flex", 
-        flexDirection: "column", 
-        height: "100vh"
-        // Removed width: "100vw" - this causes horizontal scrollbar
-    }}>
+    <Box
+        sx={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100vh",
+        }}
+    >
         <TopAppBar />
-        <Container
+        <Box
             component="main"
-            maxWidth="lg"
             sx={{
                 flexGrow: 1,
                 overflow: "auto",
                 pt: 2,
-                px: 2,
-                pb: 2
+                pb: 2,
             }}
         >
-            <Outlet />
-        </Container>
+            <Container
+                maxWidth="lg"
+                sx={{
+                    px: 2,
+                }}
+            >
+                <Outlet />
+            </Container>
+        </Box>
     </Box>
 );
 
