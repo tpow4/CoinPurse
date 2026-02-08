f0a6b64c84273bd696a5c69f760501a71993a315
Tom Powers <17350105+tpow4@users.noreply.github.com>
2026-02-07 22:21:37 -0800

    remove redirects

--- coinpurse-py/coinpurse-client/src/routes/categories/+page.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import { localizeHref } from '$lib/paraglide/runtime';
-
-export function load() {
-    redirect(301, localizeHref('/settings/categories'));
-}
